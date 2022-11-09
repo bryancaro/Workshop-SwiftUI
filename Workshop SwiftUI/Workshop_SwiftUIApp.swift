815dd17a2b0647f9f22efd222706e8facdfb47a3
Bryan Caro <bryancaro96@gmail.com>
2022-11-09 09:00:02 +0100

    Initial Commit

---
+++ Workshop SwiftUI/Workshop_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_SwiftUIApp.swift
+//  Workshop SwiftUI
+//
+//  Created by Bryan Caro on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
